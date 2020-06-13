a6339159a33af86bbe7ca1e0287df6fe17f76b9c
Omar Cobas <omarlcobas@gmail.com>
2020-06-12 20:05:55 -0400

    spotipy working

--- main.py
+++ main.py
@@ -1,26 +1,102 @@
 import discord
-import spotipy
 from discord.ext import commands
 import config as c
 import youtube_dl
-
+# import spotipy.util as util
+# from bottle import route, run, request
+# from spotipy.oauth2 import SpotifyClientCredentials
+# import requests
+# import user_playlists as playlists 
+# pip install asyncio
+# python3 -m pip install discord.py
+# python3 -m pip install spotipy
+# python3 -m pip install youtubedl
 players = {}
 
-sp = spotipy.Spotify()
 test_song = 'spotify:track:4evmHXcjt3bTUHD1cvny97'
 
+# client_credentials_manager = SpotifyClientCredentials(c.SPOTIFY_CLIENT_ID, client_secret=c.SPOTIFY_CLIENT_SECRET)
+# sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+# results = sp.search(q='Drake', limit=20)
+# for i, t in enumerate(results['tracks']['items']):
+#     print (' ', i, t['name'])
+
+import sys
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-client_credentials_manager = SpotifyClientCredentials(c.SPOTIFY_CLIENT_ID, client_secret=c.SPOTIFY_CLIENT_SECRET)
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+import spotipy.oauth2 as oauth2
 
-results = sp.search(q='Drake', limit=20)
-for i, t in enumerate(results['tracks']['items']):
-    print (' ', i, t['name'])
+import pafy
+import vlc
+
+credentials = oauth2.SpotifyClientCredentials(
+	client_id=c.SPOTIFY_CLIENT_ID,
+	client_secret=c.SPOTIFY_CLIENT_SECRET)
+
+token = credentials.get_access_token(as_dict=False)
+spotify = spotipy.Spotify(auth=token)
+playlists_table = {}
+username = ""
+
+# TODO: MAKE PLAYLISTS PUBLIC THAT YOU WANT TO BE ACCESSIBLE THROUGH THE COMMAND
+
+def show_tracks(tracks, playlists_table, playlist_name):
+    for i, item in enumerate(tracks['items']):
+        track = item['track']
+        try:
+            playlists_table[playlist_name].add((track['artists'][0]['name'], track['name']))
+        except:
+            continue
+        # print()
+        # print("   %d %32.32s %s" % (i, track['artists'][0]['name'],track['name']))
 
 
+if len(sys.argv) > 1:
+    username = sys.argv[1]
+else:
+    print("Whoops, need your username!")
+    print("usage: python user_playlists.py [username]")
+    sys.exit()
+
+
+if token:
+    sp = spotipy.Spotify(auth=token)
+    playlists = sp.user_playlists(username)
+    for playlist in playlists['items']:
+        # print()
+        playlists_table[playlist['name']] = set()
+        print(playlist['name'])
+        # print ('  total tracks', playlist['tracks']['total'])
+        results = sp.playlist(playlist['id'],
+            fields="tracks,next")
+        tracks = results['tracks']
+        show_tracks(tracks, playlists_table, playlist['name'])
+        while tracks['next']:
+            tracks = sp.next(tracks)
+            show_tracks(tracks, playlists_table, playlist['name'])
+else:
+    print("Can't get token for", username)
+
+# print('yerrrr', playlists.username)
 #sp.start_playback({"context_uri": test_song})
 client = commands.Bot(command_prefix='$')
+print(playlists_table.keys())
+print(username)
+
+
+
+youtube_URL = "https://www.youtube.com/watch?v=xWggTb45brM"
+video = pafy.new(youtube_URL)
+best = video.getbest()
+playurl = best.url
+
+Instance = vlc.Instance()
+player = Instance.media_player_new()
+Media = Instance.media_new(playurl)
+Media.get_mrl()
+player.set_media(Media)
+player.play()
+time.sleep(120)
 
 @client.event
 async def on_ready():
@@ -62,8 +138,10 @@ async def join(ctx):
 async def play(ctx):
     channel = ctx.message.author.voice.channel
     vc = await channel.connect()
-   # vc.play(sp.start_playback({"context_uri": test_song}))
-    vc.play(discord.FFmpegPCMAudio('Nick Jonas - The Difference (Audio).mp3'), after=lambda e: print('done', e))
+        
+    # print(r.content)
+    # vc.play(sp.start_playback({"context_uri": test_song}))
+    # vc.play(discord.FFmpegPCMAudio('Nick Jonas - The Difference (Audio).mp3'), after=lambda e: print('done', e))
 
 @client.command()
 async def leave(ctx):
@@ -78,4 +156,4 @@ async def yt(ctx, url):
     vc = await channel.connect()
     player = await vc.create_ytdl_player(url)
     vc.play(player)
-client.run(c.TOKEN)
+client.run(c.DISCORD_TOKEN)
